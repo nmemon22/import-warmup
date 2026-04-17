ce2504546a9ef3cb7699eb4faf5739297bf4cbb0
Jeb <nmemon94@gmail.com>
2026-04-17 12:45:54 -0400

    EDSAI-420 actually done now

---
+++ portfolio/__init__.py
@@ -0,0 +1,4 @@
+
+import sys
+import portfolio.data
+import portfolio.report
--- run_exercises.py
+++ run_exercises.py
@@ -1,6 +1,7 @@
 import sys
 # from portfolio.data import create_portfolio
-import portfolio.data
-import portfolio.report
+# import portfolio.data
+# import portfolio.report
+import portfolio
 my_portfolio = portfolio.data.create_portfolio("Retirement")
 portfolio.report.print_report(my_portfolio)
